8ea9f9bcd50fd7c24cb9b683a5170a3369973390
Rohan Kewalramani <45906368+rohankewal@users.noreply.github.com>
2023-11-15 09:31:30 -0500

    created new view for adding new shipments

---
+++ ShipTrack-MacOS/AddView.swift
@@ -0,0 +1,16 @@
+//
+//  AddView.swift
+//  ShipTrack-MacOS
+//
+//  Created by Rohan Kewalramani on 11/15/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text("This is the add view")
+            .font(.largeTitle)
+    }
+}
